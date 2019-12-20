03c1c6d5a418f56d75f68a1b429d147e000ec540
Joanna E K <kalemajoanna@gmail.com>
2019-12-20 21:53:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,6 +46,78 @@ class TicTacToe
         end
     end   
   
+   def turn_count
+        turn_num = @board.count{|space| space == "X" || space == "O"}
+    end
+
+    def current_player
+        if turn_count % 2 == 0
+            "X"
+        else
+            "O"
+        end
+    end
+
+    def turn 
+        puts "Please enter a position to place your token (b/w 1 and 9 inclusive):"
+        user_index = gets.chomp
+        array_index = input_to_index(user_index)
+        player_token = current_player
+        if valid_move?(array_index)
+            move(array_index, player_token)
+            display_board
+        else
+            puts "Sorry, that input was invalid. Please enter a position to place your token (b/w 1 and 9 inclusive): "
+            user_index = gets.chomp
+        end
+    end
+
+    def won?
+        WIN_COMBINATIONS.detect do |win_combo|
+            if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") 
+                return win_combo
+            elsif (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+                return win_combo
+            end
+            false
+        end 
+    end
+
+    def full?
+        @board.all? do |space|
+            space != " "
+        end
+    end
+
+    def draw?
+        !(won?) && full?
+    end
+
+    def over?
+        won? || draw?
+    end
+
+    def winner
+        WIN_COMBINATIONS.detect do |win_combo|
+            if (@board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X") 
+                return "X"
+            elsif (@board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O")
+                return "O"
+            end
+            nil
+        end 
+    end
+
+    def play
+        until over?
+            turn
+        end
+        if won?
+            puts "Congratulations #{winner}!"
+        elsif
+            puts "Cat's Game!"
+        end
+    end
  
 end 
 
